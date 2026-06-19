e24b5a822b91987cd58fa42677be89e8ca37f672
emily pacheco <em.pachecoq@duocuc.cl>
2026-06-19 00:12:16 -0400

    (Actualizacion) se hicieron cambion en gatawey faltan algunos cambios

--- microservicios/msvc-gateway/src/main/java/com/hospital/msvc_gateway/MsvcGatewayApplication.java
+++ microservicios/msvc-gateway/src/main/java/com/hospital/msvc_gateway/MsvcGatewayApplication.java
@@ -3,8 +3,7 @@ package com.hospital.msvc_gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-// El gateway es una app Spring Boot normal. Las rutas se definen en application.yml.
-// Al tener el cliente Eureka en el classpath, tambien se registra y puede usar lb://.
+
 @SpringBootApplication
 public class MsvcGatewayApplication {
 
